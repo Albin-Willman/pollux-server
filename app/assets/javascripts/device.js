199087c1006fb59fb0516a9fca62043c42781f7c
Jacob Burenstam <burenstam@gmail.com>
2015-02-19 19:50:34 +0100

    JS restructure

--- app/assets/javascripts/device.js
+++ app/assets/javascripts/device.js
@@ -1,54 +1,64 @@
 // Android adapter
-var PolluxAndroidAdapter = function() {
+
+var PolluxDevice = function() {
+  this.device = null;
+  if (typeof Android !== 'undefined') {
+    console.log('Running on a native Android device.');
+    device = TbAndroidAdapter;
+  } else {
+    console.log('Not running on native device.');
+    device = TbWebInterface;
+  }
+  return device;
+};
+
+
+var TbAndroidAdapter = function() {
   this.client = Android;
 
   this.showToast = function(msg) {
     client.showToast(msg);
   };
-};
 
-// Ios adapter
-var PolluxIosAdapter = function() {
-  this.client = NSWebInterface;
-
-  this.showToast = function(msg) {
-    client.NSbottomNotifyDrawerShow(msg);
+  this.requestImage = function(){
+   client.requestImage();
   };
+
+  this.showDeviceInfo = function(){
+   var deviceInfo = client.showDeviceInfo();
+
+  }
 };
 
-// TbWebInterface somewhat mimics the functions of a
-// native device such as android or iOS
+
+var device = PolluxDevice();
+console.log("Device:" + device);
+device.showToast('Hello native toaster!'); // Will show toast on both Android, iOS and Web
+// device.showDeviceInfo();
+
+
 var TbWebInterface = function() {
   this.webToast = function(msg) {
     alert(msg);
   };
-};
 
-// Web adapter
-var PolluxWebAdapter = function() {
-  this.client = TbWebInterface;
-
-  this.showToast = function(msg) {
-    return client.webToast(msg);
+  this.requestImage = function(){
+   alert("Not supported yet");
+  };
+  this.showDeviceInfo = function(){
+   alert("Not supported yet");
   };
 
-}
-
-// PolluxDevice interface
-var PolluxDevice = function() {
-  this.device = null;
-
-  if (typeof Android !== 'undefined') {
-    console.log('Running on a native Android device.');
-    device = TbAndroidAdapter;
-  } else if (typeof NSWebInterface !== 'undefined') {
-    console.log('Running on a native iOS device.');
-    device = TbIosAdapter;
-  } else {
-    console.log('Not running on native device.');
-    device = TbWebInterface;
-  }
 };
 
-var device = new PolluxDevice().device;
-device.showToast('Hello native toaster!'); // Will show toast on both Android, iOS and Web
+function showToast(toast) {
+    device.showToast(toast);
+}
+
+function requestImage(){
+ device.requestImage();
+}
+
+function addImgBase64(base64){
+ document.getElementsByTagName("img")[0].src="data:image/jpeg;base64," + base64;
+}
