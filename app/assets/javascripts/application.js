9b65590836a93dcc044a33d83795ad910784a59d
pbl <philip.burenstam.linder@gmail.com>
2015-02-19 13:53:47 +0100

    asd

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,69 +15,89 @@
 //= require_tree .
 
 
-
-
-var PolluxDevice = function() {
-  this.device = null;
-  if (typeof Android !== 'undefined') {
-    console.log('Running on a native Android device.');
-    device = TbAndroidAdapter;
-  } else {
-    console.log('Not running on native device.');
-    device = TbWebInterface;
-  }
-  return device;
-};
-
-
-var TbAndroidAdapter = function() {
-  this.client = Android;
-
-  this.showToast = function(msg) {
-    client.showToast(msg);
-  };
-
-  this.requestImage = function(){
-  	client.requestImage();
-  };
-
-  this.showDeviceInfo = function(){
-  	var deviceInfo = client.showDeviceInfo();
-
-  }
-};
-
-
 function showToast(toast) {
-    device.showToast(toast);
-} 	
-
-var device = PolluxDevice();
-console.log("Device: " + device);
-device.showToast('Hello native toaster!'); // Will show toast on both Android, iOS and Web
-// device.showDeviceInfo();
-
-
-var TbWebInterface = function() {
-  this.webToast = function(msg) {
-    alert(msg);
-  };
-
-  this.requestImage = function(){
-  	alert("Not supported yet");
-  };
-  this.showDeviceInfo = function(){
-  	alert("Not supported yet");
-  };
-
-};
-
-
+    Android.showToast(toast);
+} 
 
 function requestImage(){
-	device.requestImage();
+	Android.requestImage();
 }
 
 function addImgBase64(base64){
 	document.getElementsByTagName("img")[0].src="data:image/jpeg;base64," + base64;
 }
+
+function getDeviceInfo(){
+  	var deviceInfoUnformated = Android.showDeviceInfo();
+  	console.log(deviceInfoUnformated);
+  	// var deviceInfo = JSON.parse(deviceInfoUnformated);
+  	// console.log(deviceInfo);
+ }
+
+
+
+
+// var PolluxDevice = function() {
+//   this.device = null;
+//   if (typeof Android !== 'undefined') {
+//     console.log('Running on a native Android device.');
+//     device = TbAndroidAdapter;
+//   } else {
+//     console.log('Not running on native device.');
+//     device = TbWebInterface;
+//   }
+//   return device;
+// };
+
+
+// var TbAndroidAdapter = function() {
+//   this.client = Android;
+
+//   this.showToast = function(msg) {
+//     client.showToast(msg);
+//   };
+
+//   this.requestImage = function(){
+//   	client.requestImage();
+//   };
+
+//   this.showDeviceInfo = function(){
+//   	var deviceInfo = client.showDeviceInfo();
+
+//   }
+// };
+
+
+	
+
+// var device = PolluxDevice();
+// console.log("Device:" + device);
+// device.showToast('Hello native toaster!'); // Will show toast on both Android, iOS and Web
+// // device.showDeviceInfo();
+
+
+// var TbWebInterface = function() {
+//   this.webToast = function(msg) {
+//     alert(msg);
+//   };
+
+//   this.requestImage = function(){
+//   	alert("Not supported yet");
+//   };
+//   this.showDeviceInfo = function(){
+//   	alert("Not supported yet");
+//   };
+
+// };
+
+// function showToast(toast) {
+//     device.showToast(toast);
+// } 
+
+// function requestImage(){
+// 	device.requestImage();
+// }
+
+// function addImgBase64(base64){
+// 	document.getElementsByTagName("img")[0].src="data:image/jpeg;base64," + base64;
+// }
