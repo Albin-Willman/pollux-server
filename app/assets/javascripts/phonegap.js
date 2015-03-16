50b6e23df9f7592919e4849e8f3e23046403fffc
pbl <philip.burenstam.linder@gmail.com>
2015-03-16 15:10:37 +0100

    auto push

--- app/assets/javascripts/phonegap.js
+++ app/assets/javascripts/phonegap.js
@@ -1,3 +1,8 @@
+$( document ).ready(function() {
+    window.addEventListener('message', wizMessageReceiver);
+});
+
+
 function wizMessageReceiver (event) {
    	$("#phonegap-msg").append(event);
 }
