9a6fce0b4eea0a5bb2b716f21b449dd3c18f02e0
David <david.norrestam@hotmail.com>
2015-03-19 12:30:39 +0100

    auto push

--- app/assets/javascripts/phonegap.js
+++
@@ -1,8 +0,0 @@
-$( document ).ready(function() {
-    window.addEventListener('message', wizMessageReceiver, false);
-});
-
-function wizMessageReceiver (event) {
-	$("#phonegap-msg").removeText();
-   	$("#phonegap-msg").text("PHONEGAP OMG OMG OMG");
-}
