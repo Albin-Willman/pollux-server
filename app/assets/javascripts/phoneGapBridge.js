5d1ef6bbcd3a03811946a0e422d50cdf97a512f8
David <david.norrestam@hotmail.com>
2015-03-20 12:37:57 +0100

    auto push

--- app/assets/javascripts/phoneGapBridge.js
+++ app/assets/javascripts/phoneGapBridge.js
@@ -3,7 +3,7 @@ var bridge = {
 		console.log("webclient, bridge: sent imagerequest");
 		sender.requestImage();
 	},
-	
+
 	requestGeolocation: function() {
 		console.log("webclient, bridge: sent geolocationRequest");
 		sender.requestGeolocation();
@@ -15,14 +15,5 @@ var bridge = {
 
 	geolocationCallback: function (data) {
 		var locationJSON = JSON.parse(data);
-
-			'Latitude: '          + position.coords.latitude          + '\n' +
-			'Longitude: '         + position.coords.longitude         + '\n' +
-			'Altitude: '          + position.coords.altitude          + '\n' +
-			'Accuracy: '          + position.coords.accuracy          + '\n' +
-			'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-			'Heading: '           + position.coords.heading           + '\n' +
-			'Speed: '             + position.coords.speed             + '\n' +
-			'Timestamp: '         + position.timestamp                + '\n');
 	}
 }
