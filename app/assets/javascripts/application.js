82fa519878f7aec9c6ea461d1ceb7851e4b893c1
Jacob Burenstam <burenstam@gmail.com>
2015-03-22 20:08:53 +0100

    Cleanup

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -27,11 +27,6 @@ function debug(msg) {
 $(document).ready(function() {
   var localMediaStream = null;
 
-  $('#take-picture').on('click', function(e) {
-    e.preventDefault();
-    Pollux.device.requestCamera('addImgBase64');
-  });
-
   $('#upload-image').on('click', function(e) {
     e.preventDefault();
     Pollux.device.requestImage('addImgBase64');
