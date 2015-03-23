d17b1f873f39aed037008036e59fe7fc59b11165
pbl <philip.burenstam.linder@gmail.com>
2015-03-23 16:32:41 +0100

    auto push

--- app/assets/javascripts/device.js
+++ app/assets/javascripts/device.js
@@ -115,27 +115,21 @@
       $('#fileUpload').change(function(){
         var regex = /^([a-zA-Z0-9\s_\\.\-:])+(.jpg|.jpeg|.gif|.png|.bmp)$/;
         if (regex.test($(this).val().toLowerCase())) {
-          // if ($.browser.msie && parseFloat(jQuery.browser.version) <= 9.0) {
+          if (typeof (FileReader) != "undefined") {
             // $("#dvPreview").show();
-            // $("#dvPreview")[0].filters.item("DXImageTransform.Microsoft.AlphaImageLoader").src = $(this).val();
-          // }
-          // else {
-            if (typeof (FileReader) != "undefined") {
-              $("#dvPreview").show();
-              $("#dvPreview").append("<img />");
-              var reader = new FileReader();
-              reader.onload = function (e) {
-                $("#dvPreview img").attr("src", e.target.result);
-              }
-              reader.readAsDataURL($(this)[0].files[0]);
-            } else {
-              alert("This browser does not support FileReader.");
+            // $("#dvPreview").append("<img />");
+            var reader = new FileReader();
+            reader.onload = function (e) {
+              $("#captured-image").attr("src", e.target.result);
             }
-          // }
+            reader.readAsDataURL($(this)[0].files[0]);
+          } else {
+            alert("This browser does not support FileReader.");
+          }
       } else {
           alert("Please upload a valid image file.");
         }
-      });
+      });$.browser.msie
 
     };
 
