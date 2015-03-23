de46e7f2aa99ef349e10bf8b69c09e5961d8516e
pbl <philip.burenstam.linder@gmail.com>
2015-03-23 16:30:05 +0100

    auto push

--- app/assets/javascripts/device.js
+++ app/assets/javascripts/device.js
@@ -115,11 +115,11 @@
       $('#fileUpload').change(function(){
         var regex = /^([a-zA-Z0-9\s_\\.\-:])+(.jpg|.jpeg|.gif|.png|.bmp)$/;
         if (regex.test($(this).val().toLowerCase())) {
-          if ($.browser.msie && parseFloat(jQuery.browser.version) <= 9.0) {
-            $("#dvPreview").show();
-            $("#dvPreview")[0].filters.item("DXImageTransform.Microsoft.AlphaImageLoader").src = $(this).val();
-          }
-          else {
+          // if ($.browser.msie && parseFloat(jQuery.browser.version) <= 9.0) {
+            // $("#dvPreview").show();
+            // $("#dvPreview")[0].filters.item("DXImageTransform.Microsoft.AlphaImageLoader").src = $(this).val();
+          // }
+          // else {
             if (typeof (FileReader) != "undefined") {
               $("#dvPreview").show();
               $("#dvPreview").append("<img />");
@@ -131,7 +131,7 @@
             } else {
               alert("This browser does not support FileReader.");
             }
-          }
+          // }
       } else {
           alert("Please upload a valid image file.");
         }
